12f1aa538e9c16baf79203abf56be85adeb70c99
ProximalBeets <nake73a@gmail.com>
2023-11-25 18:55:15 -0500

    Code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.math.controller.PIDController;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +16,38 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+
+public final class Constants {
+     // Motors
+     private static Spark LEFT_FRONT_DRIVE_SPEED_MOTOR;
+     private static Spark LEFT_BACK_DRIVE_SPEED_MOTOR;
+     private static Spark RIGHT_FRONT_DRIVE_SPEED_MOTOR;
+     private static Spark RIGHT_BACK_DRIVE_SPEED_MOTOR;
+ 
+     private static Spark LEFT_FRONT_DRIVE_DIRECTION_MOTOR;
+     private static Spark LEFT_BACK_DRIVE_DIRECTION_MOTOR;
+     private static Spark RIGHT_FRONT_DRIVE_DIRECTION_MOTOR;
+     private static Spark RIGHT_BACK_DRIVE_DIRECTION_MOTOR;
+ 
+     // Encoders
+     public static Encoder LEFT_FRONT_DRIVE_DISTANCE_ENCODER;
+     public static Encoder LEFT_BACK_DRIVE_DISTANCE_ENCODER;
+     public static Encoder RIGHT_FRONT_DRIVE_DISTANCE_ENCODER;
+     public static Encoder RIGHT_BACK_DRIVE_DISTANCE_ENCODER;
+     //public static MedianPIDSource DRIVE_DISTANCE_ENCODERS;
+ 
+     public static Encoder LEFT_FRONT_DRIVE_DIRECTION_ENCODER;
+     public static Encoder LEFT_BACK_DRIVE_DIRECTION_ENCODER;
+     public static Encoder RIGHT_FRONT_DRIVE_DIRECTION_ENCODER;
+     public static Encoder RIGHT_BACK_DRIVE_DIRECTION_ENCODER;
+ 
+     // Direction encoder wrapper that scales to degrees
+     //public static PIDSourceExtended LEFT_FRONT_DRIVE_DIRECTION_SCALED;
+     //public static PIDSourceExtended LEFT_BACK_DRIVE_DIRECTION_SCALED;
+     //public static PIDSourceExtended RIGHT_FRONT_DRIVE_DIRECTION_SCALED;
+     //public static PIDSourceExtended RIGHT_BACK_DRIVE_DIRECTION_SCALED;
+ 
+     // Gyro
+     //Pigeon2 _pigeon = new Pigeon2(0, "rio");
+
+}
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -6,8 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Swerve extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
+public class Swerve extends SubsystemBase {  /** Creates a new ExampleSubsystem. */
   public Swerve() {}
 
   @Override
